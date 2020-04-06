de2a0cd3f0fb4afa9e9275cb237affc62d1e4f78
benholmgren <b.holmgren3@gmail.com>
2020-04-06 14:41:33 -0600

    add proj 1

---
+++ proj1/build/CMakeFiles/proj1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj1.dir/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glad/src/glad.c.o"
+  "CMakeFiles/proj1.dir/main.cpp.o"
+  "proj1"
+  "proj1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/proj1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ proj1/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/cocoa_init.m" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/cocoa_joystick.m" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/cocoa_monitor.m" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/cocoa_time.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/cocoa_window.m" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/context.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/egl_context.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/init.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/input.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/monitor.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/nsgl_context.m" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/osmesa_context.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/posix_thread.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/vulkan.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src/window.c" "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/proj1shading/build/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/include"
+  "/Users/benjaminholmgren/Desktop/Courses/Spring2020/Graphics/csci_441/deps/glfw/src"
+  "glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
